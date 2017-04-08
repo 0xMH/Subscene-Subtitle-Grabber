1e941aa9204743598ba20b0ca5a85defe9598a8e
Rafay Ghafoor <rafayghafoor@protonmail.com>
2017-04-08 22:55:39 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
-from SubsceneDL import *
+from distutils.core import setup
 
-
-if __name__=="__main__":
-    main()
+setup(
+    name = 'sub-dl',
+    packages = ['sub-dl'],
+    version = '0.1',  # Ideally should be same as your GitHub release tag varsion
+    description = 'A tool to download Subtitles from subscene site.',
+    author = 'Rafay Ghafoor',
+    author_email = 'rafayghafoor@protonmail.com',
+    url = 'https://github.com/RafayGhafoor/Subscene-Subtitle-Grabber',
+    download_url = 'download link you saved',
+    keywords = ['python', 'srt'],
+    classifiers = [],
+)
