ac3b4a956dc51c488f5ffa1339a7119d0bc24733
Rafay Ghafoor <rafayghafoor@protonmail.com>
2017-04-09 13:16:37 +0500

    docs

--- SubsceneDL.py
+++ SubsceneDL.py
@@ -19,8 +19,12 @@ from time import sleep
 real_directory = []
 ext = ['.mp4', '.mkv', '.avi']
 
-def createFolder():
-    # Create Folders For Movies
+def create_folder():
+    '''Search for MP4, MKV, AVI extensions inside the current
+    directory and If any of the files ending with such
+    extensions are found (not in folder), create folder
+    for them and paste the respective file in the corresponding
+    folder.'''
     global ext
     for files in os.listdir('.'):
         for extension in ext:
@@ -34,26 +38,36 @@ def createFolder():
                 pass
 
 
-def deepSearch():
-    # Slow But More Precise Method.
-    # If Len of title matches about 80 % of the search query name download the subtitle
+def simple_search():
+    '''Takes the name of the file (containing metadata)
+    for example:-
+    >>> Doctor.Strange.2016.BrRip.720p.YIFY.mp4
+    and search the name on subscene site, if subtitle found for
+    this name, download subtitles for it. Preffered for (Auto-Sync).'''
     pass
 
 
-def simpleSearch():
-    # Fast But Lesss Precise Method.
-    '''Takes Direct Movie Name for eg: Doctor.Strange.2016.Bluray.H.264-Nezu.mp4
-    and Search on the site, if found Download Subtitle for it'''
+def get_year(filename):
+    '''Obtains year from the movie filename.
+    For example:-
+    >>> Doctor.Strange.2016.BrRip.720p.YIFY.mp4
+    gets (2016) from the media file.'''
+    yearRegex = re.compile(r'\d{4}')
+    searchItems = yearRegex.search(filename)
+    year = searchItems.group()
+    return year
+
+def get_imdb_year():
+    '''If move file doesn't contain year then searches the movie
+    on IMDB and obtains year. For better precision, movie runtime
+    is obtained and then on IMDB the movie name is searched and if
+    the movie runtime matches with the movie (on IMDB), year is obtained.'''
     pass
 
 
-def imdbYearObtainer():
-    # If there is no Year in the Movie Name File, Get the Year From Imdb by checking the Runtime of the movie
-    pass
-
-
-def getMovieRuntime(moviefile):
-    # Obtains movie Runtime For Cross-Check from IMDB i.e. Obtaining Movie Year
+def get_movie_runtime(moviefile):
+    '''Gets Movie Runtime for better search at IMDB site for
+    obtaining movie year.'''
     cmd = 'ffprobe -show_entries format=duration -v quiet -of csv="p=0"'
     args = shlex.split(cmd)  # Creating a List of the Command and its Parameters
     args.append(file_path_with_file_name)  # Appending File to the Arguments list
@@ -66,7 +80,9 @@ def getMovieRuntime(moviefile):
     print 'Hours %s and Minutes %s' % (hours, minutes)
 
 
-def nameFinder(name, year, parameter):
+def name_finder(name, year, parameter):
+    '''Searches for movie on IMDB site. If [Name and Year] (Of Movie)
+    are matched on Subscene site, movie (title) is obtained and searched in.'''
     r = requests.get(search, {'q': parameter})
     print 'Generated URL is ---> %s\n' % r.url
     soup = bs4.BeautifulSoup(r.content, 'html.parser')
@@ -87,8 +103,8 @@ def nameFinder(name, year, parameter):
         return 'https://subscene.com' + foundUrl
 
 
-def downLinkFinder(page_link, count = 1):
-    # Interaction with Site using Requests
+def downlink_finder(page_link, count = 1):
+    '''Finds Download Links on the obtained page from Name Finder (function).'''
     r = requests.get(page_link)
     sub_list = []   # Subtitles List
     # Searching Page For Getting the Correct Movie Name
@@ -105,8 +121,8 @@ def downLinkFinder(page_link, count = 1):
     return ['https://subscene.com' + i for i in sub_list]
 
 
-def zipExtractor(name):
-    # Extracts Zip File Downloaded from Subscene
+def zip_extractor(name):
+    '''Extracts zip file obtained from the Subscene site (which contains Subtitles)'''
     try:
         with zipfile.ZipFile(name, "r") as z:
             z.extractall(".")
@@ -115,6 +131,7 @@ def zipExtractor(name):
         pass
 
 def downloader(dl_links):
+    '''Downloads Subtitles for the links obtained from the Download Link Finder (function)'''
     r = requests.get(dl_links)
     soup = bs4.BeautifulSoup(r.content, 'html.parser')
     for div in soup.find_all('div', {'class': 'download'}):
@@ -130,9 +147,13 @@ def downloader(dl_links):
                     chunk_size=150):
                 if chunk:
                     f.write(chunk)
-        zipExtractor(name)
+        zip_extractor(name)
 
-def removeExtension(name):
+def remove_extension(name):
+    '''Removes extension from the movie file name.
+    For example:-
+    >>> Doctor.Strange.2016.mp4
+    becomes Doctor.Strange.2016'''
     global ext
     newvar = ''
     for extension in ext:
@@ -147,23 +168,24 @@ def removeExtension(name):
         return newvar
 
 
-def movieSubDL(mediaName = '', mediaYear = ''):
+def movie_subdl(mediaName = '', mediaYear = ''):
+    '''A function which runs Name Finder, Download link Finder and
+    Downloader functions.'''
     # For Downloading Subtitle For Required Movie
-    mediaName = removeExtension(mediaName) # Removes Extension eg. --> .mp4
-    if mediaYear != '':
-        mediaName = mediaName.strip(mediaYear)
-    parameters = mediaName.strip(' ')
-    query = nameFinder(mediaName, mediaYear, parameters)  # List Of Elements
+    mediaName = remove_extension(mediaName) # Removes Extension eg. --> .mp4
+    parameters = mediaName + ' ' + mediaYear
+    parameters = parameters.strip(' ')
+    query = name_finder(mediaName, mediaYear, parameters)  # List Of Elements
     if query != None:
-        downlinks = downLinkFinder(query, 1)
+        downlinks = downlink_finder(query, 1)
         for elements in downlinks:
-            print '[*] - Downloading Subtitle For %s' % (mediaName.title() + mediaYear)
+            print '[*] - Downloading Subtitle For %s' % (mediaName + mediaYear)
             downloader(elements)
             print '[+] - Subtitle Downloaded!'
 
 
-def nameGrabber(medialst):
-    # Gets the Name of the movie whose subtitle needs to be Downloaded!
+def name_grabber(medialst):
+    '''Gets the name of the movies whose subtitles are to be Downloaded.'''
     nameslist = []
     for movies in medialst:
         try:
@@ -173,17 +195,18 @@ def nameGrabber(medialst):
             # This is 2016 Movie --> This is | 2016 | Movie
             prev, found, removal = movies.partition(year)
             if prev[-1] == ' ':
-                nameslist.append(prev.lower() + found)
+                nameslist.append(prev.lower())
             else:
-                nameslist.append(prev.lower()[:-1] + found)
+                nameslist.append(prev.lower()[:-1])
         except:
-            nameslist.append(movies.lower() + found)
+            nameslist.append(movies.lower())
             continue
     nameslist = [i.strip(' ') for i in nameslist]
     return nameslist
 
 
-def directoryObtainer():
+def directory_obtainer():
+    '''Gets movies names contained in a directory.'''
     global real_directory
     global ext
 
@@ -198,7 +221,9 @@ def directoryObtainer():
     return real_directory
 
 
-def fileLocator(name):
+def file_locator(name):
+    '''Locates the file for moving the downloading subtitles to the
+    right place.'''
     for files in os.listdir('.'):
         if files.endswith('.py') == False:
             # if os.path.isdir(elements) == True:
@@ -213,21 +238,22 @@ def fileLocator(name):
                     break
 
 
-def directorySubDL(movieNames, movieDirectory):
+def directory_subdl(movieNames, movieDirectory):
+    '''Downloads Subtitles for the Directory.'''
     # For Downloading Subtitles for movies in a Directory
     cwd = os.getcwd()
     num = 0
     for elements in movieNames:
         # os.chdir(elements)
-        location = fileLocator(real_directory[num])
+        location = file_locator(real_directory[num])
         os.chdir(location)
         try:
             yearRegex = re.compile(r'\d{4}')
-            searchItems = yearRegex.search(elements)
+            searchItems = yearRegex.search(movies)
             movieYear = searchItems.group()
         except:
             movieYear = ''
-        movieSubDL(elements, movieYear)
+        movie_subdl(elements, movieYear)
         os.chdir(cwd)
         if num != len(real_directory) - 1:
             num += 1
@@ -235,10 +261,12 @@ def directorySubDL(movieNames, movieDirectory):
             break
 
 
-def subChecker(directory):
+def sub_checker(directory):
+    '''Checks for the subtitles if they are already in the movie folder, it
+    skips the movie.'''
     for folders, subfolders, files in os.walk('.'):
         for elements in files:
-            if elements.endswith('.srt') or elements.endswith('.ass'):
+            if elements.endswith('.srt'):
                 try:
                     for movies in files:
                         if movies.endswith('.mkv') or movies.endswith('.avi') or movies.endswith('.mp4'):
@@ -250,30 +278,30 @@ def subChecker(directory):
                     directory.remove(elements)
                 except:
                     pass
-    directory = [i.replace('.', ' ') for i in directory]
     return directory
 
 
 
-def subRenamer():
-    # Rename Subtitle Downloaded To the Media File Name for auto-Sync
+def sub_renamer():
+    '''Rename the downloaded subtitles to the movie name for auto-sync with
+    media player.'''
     pass
 
 
-def main():
+if __name__ == "__main__":
     makeChoice = int(raw_input("Press [1] - For Downloading Subtitles in A Directory.\n\
 Press [2] - For Download Subtitles For a Custom Movie:\n\
 Your Input [-]:  "))
     search = "https://subscene.com/subtitles/title"
     if makeChoice == 1:
-        createFolder()
-        real_directory = directoryObtainer()
-        newdir = subChecker(real_directory)
-        names = nameGrabber(newdir)
-        directorySubDL(names, newdir)
+        create_folder()
+        real_directory = directory_obtainer()
+        newdir = sub_checker(real_directory)
+        names = name_grabber(newdir)
+        directory_subdl(names, newdir)
     elif makeChoice == 2:
         movieName = raw_input('Enter Movie Name: ')
         movieYear = raw_input('Enter Movie Release Year: ')
-        movieSubDL(movieName, movieYear)
+        movie_subdl(movieName, movieYear)
     else:
         print 'Invalid Choice!'
